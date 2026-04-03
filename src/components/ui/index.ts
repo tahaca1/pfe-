3c8c6362b7b110edef7ea1d706ace5c7cd829caf
taha ben rayana <tahabenrayanaca@gmail.com>
2026-04-03 20:33:25 +0200

    Delete src directory

--- src/components/ui/index.ts
+++
@@ -1,5 +0,0 @@
-export { Button } from './Button';
-export { Card } from './Card';
-export { Input, Textarea } from './Input';
-export { Slider, Badge, Chip } from './Slider';
-export { Modal, Select } from './Modal';
